9bb8174a2d09a6399650baaea95dbffdeb7b1aab
何龙 <hel04@mingyuanyun.com>
2018-07-30 20:58:20 +0800

    add config

---
+++ helpers/Config.py
@@ -0,0 +1,16 @@
+import os
+import ConfigParser
+
+class Config:
+    def __init__(self, path=''):
+        if path == '':
+            path = os.path.dirname(__file__) + '/../config/config.ini'
+        self.path = path
+
+    def get_config(self, sections, key, default=''):
+        cf = ConfigParser.ConfigParser()
+        cf.read(self.path)
+        value = cf.get(sections, key)
+        if value == '':
+            return default
+        return value
