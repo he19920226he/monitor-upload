908a43e497b98d91c83938cdbc7977fe1d76b2f5
何龙 <hel04@mingyuanyun.com>
2019-07-30 17:04:05 +0800

    开始玩窗口

--- main.py
+++ main.py
@@ -3,22 +3,25 @@ from helpers.Config import Config
 from helpers.Sftp import Sftp
 from helpers.Watchdog import Watchdog
 import time
+from windows.HomePage import HomePage
 
 if __name__ == "__main__":
-    config = Config()
-    # 获取要监控的目录
-    local_path = config.get_config('document', 'localPath', '')
-    print('watch dir:' + local_path + '...................')
-    # 获取sftp配置
-    remote_path = config.get_config('document', 'remotePath', '')
-    if local_path == '' or remote_path == '':
-        print('path error..................')
-        time.sleep(5)
-        exit()
-    # sftp链接
-    sftp = Sftp(remote_path)
-    sftp.check_remote_path()
-    # watchdog监控文件
-    dog = Watchdog(local_path, sftp)
-    dog.start()
+    # config = Config()
+    # # 获取要监控的目录
+    # local_path = config.get_config('document', 'localPath', '')
+    # print('watch dir:' + local_path + '...................')
+    # # 获取sftp配置
+    # remote_path = config.get_config('document', 'remotePath', '')
+    # if local_path == '' or remote_path == '':
+    #     print('path error..................')
+    #     time.sleep(5)
+    #     exit()
+    # # sftp链接
+    # sftp = Sftp(remote_path)
+    # sftp.check_remote_path()
+    # # watchdog监控文件
+    # dog = Watchdog(local_path, sftp)
+    # dog.start()
+    homePage = HomePage()
+    homePage.index()
 
---
+++ windows/HomePage.py
@@ -0,0 +1,9 @@
+# -- coding: utf-8 --
+from tkinter import *
+
+
+class HomePage:
+    def index(self):
+        gui = Tk(className="SFTP自动上传工具")
+        label = Label()
+        mainloop()
